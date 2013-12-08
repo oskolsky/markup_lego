3aa097d8aab45807551771d82a4286c74b514e12
Oskolsky Maxim <info@oskolsky.com>
2013-12-08 16:46:16 +0400

    Install new template

---
+++ assets/javascripts/project.js
@@ -0,0 +1,159 @@
+
+$(function() {
+  
+  $.stickyHeader();
+  $.stickyFooter();
+
+
+
+  //****************************************************************************************************
+  //
+  // .. DOUBLE HOVER
+  //
+  //****************************************************************************************************
+  doubleHover('a.double-hover', 'hover');
+
+
+
+  //****************************************************************************************************
+  //
+  // .. FORMS
+  //
+  //****************************************************************************************************
+  $('.form').customForm(); // $('#checkbox').customForm() to init single element; $('body').customForm() to init all elements
+
+
+
+  //****************************************************************************************************
+  //
+  // .. SCROLL TO
+  //
+  //****************************************************************************************************
+  $('a[data-scroll="true"]').on('click touchend', function() {
+    var       $this = $(this),
+             anchor = $this.attr('href'),
+        destination = $(anchor).offset().top;
+    $('html, body').animate({scrollTop: destination}, 500);
+    return false;
+  });
+  
+
+
+  //****************************************************************************************************
+  //
+  // .. SLIDERS
+  //
+  //****************************************************************************************************
+  $('#cycle-2-slider').cycle({
+    centerHorz: true,
+    centerVert: true,
+    speed: 1000,
+    swipe: true,
+    timeout: 3000
+  });
+
+
+
+  //****************************************************************************************************
+  //
+  // .. DIALOGS
+  //
+  //****************************************************************************************************
+  //
+  // .. Open dialog
+  //
+  $('[data-dialog]').on('click touchend', function() {
+    var $this = $(this),
+           id = $this.data('dialog');
+    $.arcticmodal({
+      type: 'ajax',
+      url: '/views/dialogs/_' + id + '.html'
+    });
+    return false;
+  });
+
+  //
+  // .. Close dialog
+  //
+  $(document).on('click touchend', '.js-dialog_close', function() {
+    $.arcticmodal('close');
+  });
+
+
+
+  //****************************************************************************************************
+  //
+  // .. ACCOUNTING
+  //
+  //****************************************************************************************************
+  //
+  // .. Number
+  //
+  $('.format-number').each(function() {
+    var
+      number = parseInt($(this).text()),
+      formatNumber = accounting.formatNumber(number);
+
+    $(this).text(formatNumber);
+  });
+
+  //
+  // .. Money
+  //
+  $('.format-money').each(function() {
+    var c = accounting.settings.currency;
+
+    if ($(this).hasClass('__rub')) {
+      c.format = '%v';
+    } else if ($(this).hasClass('__usd')) {
+      c.symbol = '$';
+      c.format = '%s%v';
+    } else if ($(this).hasClass('__eur')) {
+      c.symbol = '€';
+      c.format = '%s%v';
+    }
+
+    var
+      number = parseFloat($(this).text()),
+      formatMoney = accounting.formatMoney(number);
+    
+    if ($(this).hasClass('__rub')) {
+      $(this).text(formatMoney).append('&nbsp;<i class="fa fa-ruble"></i>');
+    } else {
+      $(this).text(formatMoney);
+    }
+  });
+
+
+
+  //****************************************************************************************************
+  //
+  // .. LOAD
+  //
+  //****************************************************************************************************
+  $(window).load(function() {});
+
+
+
+  //****************************************************************************************************
+  //
+  // .. SCROLL
+  //
+  //****************************************************************************************************
+  $(window).scroll(function() {});
+
+
+
+  //****************************************************************************************************
+  //
+  // .. RESIZE
+  //
+  //****************************************************************************************************
+  $(window).smartresize(function() {
+
+    $.stickyHeader();
+    $.stickyFooter();
+
+  });
+  
+});
---
+++ readme.md
@@ -0,0 +1,14 @@
+![alt text](/logo.png "Chemistry Template")
+
+# Chemistry Template
+
+Version: 0.7
+
+Updated: 05/12/2013
+
+Works with Sword: https://github.com/somu/sword
+
+## Credits
+
+* @brainsapiens — Senior Frontend Developer
+* @oskolsky — Senior Frontend Developer
